0953dc0d80d5b5d1a144a840620c40b3c8f6429f
Tony Sæle <zeddy@Tonys-MacBook-Pro.local>
2019-12-28 22:00:51 +0100

    ragequit

--- advent/day18.v
+++ advent/day18.v
@@ -170,24 +170,71 @@ fn fastest_dungeon_crawl(dungeon grid.Grid) DungeonCrawl {
     starts[i].doors << path.to
   }
 
+  // Remove terminals that are just doors
+  mut blacklisted := []byte
+  mut i := 0
+  for i < terminals.len {
+    t := terminals[i]
+    if t >= `a` && t <= `z` {
+      i++
+      continue
+    }
+
+    if !(t in blacklisted) {
+      blacklisted << t
+    }
+
+    path := find_key_distance(starts, `@`, t) or { panic(err) }
+    if path.doors.len < 2 {
+      terminals.delete(i)
+    } else {
+      terminals[i] = path.doors[path.doors.len - 2]
+    }
+  }
+
+  // Remove duplicates
+  i = 0
+  mut seen := []byte
+  for i < terminals.len {
+    t := terminals[i]
+    if t in seen {
+      terminals.delete(i)
+    } else {
+      seen << t
+      i++
+    }
+  }
+
+  // Remove terminals that aren't
+  mut doors := []byte
+  for t in terminals {
+    path := find_key_distance(starts, `@`, t) or { panic(err) }
+    doors << path.doors[..(path.doors.len - 1)]
+  }
+  i = 0
+  for i < terminals.len {
+    t := terminals[i]
+    if t in doors {
+      terminals.delete(i)
+    } else {
+      i++
+    }
+  }
+
   // Add all the nodes we need to consider
   // 1. The next node in the path from the current node (node is a key or door)
   // 2. The door unlocked by any given key
   // 3. The first key in all other paths from each path's terminal
   // This will lead to some duplicates, which is not a big deal
-  println(keys)
-  println(terminals)
   mut paths := []KeyDistance
   for t in terminals {
     mut path := find_key_distance(starts, `@`, t) or { panic(err) }
-    println(path)
-
     mut current := `@`
     mut nexts := starts
     for next in path.doors {
       // 1. Stride from node to next node in path
       mut to_next := find_key_distance(nexts, current, next) or { panic(err) }
-      to_next.terminal = t
+      to_next.terminal = path.to
       nexts = dungeon_key_distances(dungeon, next)
       for i, n in nexts {
         nexts[i].doors << n.to
@@ -196,9 +243,11 @@ fn fastest_dungeon_crawl(dungeon grid.Grid) DungeonCrawl {
       current = next
       if next >= `a` && next <= `z` {
         // 2. Path from key to door
-        mut to_door := find_key_distance(nexts, next, next - 0x20) or { continue }
+        door := next - 0x20
+        if door in blacklisted { continue }
+        mut to_door := find_key_distance(nexts, next, door) or { continue }
         for start in starts {
-          if (next - 0x20) in start.doors { to_door.terminal = start.to }
+          if door in start.doors { to_door.terminal = start.to }
         }
         paths << to_door
       }
@@ -206,25 +255,26 @@ fn fastest_dungeon_crawl(dungeon grid.Grid) DungeonCrawl {
 
     // 3. The first key in all other paths from the path's terminal
     for t2 in terminals {
-      if t == t2 { continue }
+      if path.to == t2 { continue }
       t2_path := find_key_distance(starts, `@`, t2) or { panic(err) }
       start := t2_path.doors[0]
-      mut t_to_start := find_key_distance(nexts, t, start) or { panic(err) }
+      mut t_to_start := find_key_distance(nexts, path.to, start) or {
+        panic(err)
+      }
       t_to_start.terminal = t2
       paths << t_to_start
     }
   }
-
-  // TODO: Prune terminals that are empty doors
-
-  // for path in paths { println(path) }
 
   crawl := DungeonCrawl {
     paths: paths
     at: `@`
   }
 
-  return crawl.best_crawl()
+  for path in paths {
+    println(path)
+  }
+  return crawl//.best_crawl()
 }
 
 fn run_day18(filename string) int {
